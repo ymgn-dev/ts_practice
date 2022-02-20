fb4184034ea9db20d94567128ccc260579e18d22
ymgn9314 <yamagen.private@gmail.com>
2022-02-20 23:59:45 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 console.log('Hello, TypeScript!')
 
 /// tsの型アノテーションはできる限り使用せず、推論させるのが良い
-let a = 'hello' // string
-let an: string = 'hello' // string(型アノテーション)
+let hello = 'hello' // string
+let helloWithType: string = 'hello' // string(型アノテーション)
+
+// オブジェクトの型アノテーションは:objectとしてはいけない
+// この場合の型推論は{ b : string }となるため
+// またjsのオブジェクトはミュータブルのため、constで宣言しても
+// オブジェクトの中身までリテラル型にならない
+let a = {
+  b: 'x',
+}
+
+a.b
