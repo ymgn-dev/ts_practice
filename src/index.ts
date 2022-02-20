800dc497b9d59bf68e23e93ca9f4fe9c1d7a7131
ymgn9314 <yamagen.private@gmail.com>
2022-02-21 00:04:52 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,16 @@ let a = {
 }
 
 a.b
+
+let c: {
+  firstName: string
+  lastName: string
+} = {
+  firstName: 'john',
+  lastName: 'barrowman',
+}
+
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+}
+c = new Person('matt', 'smith')
